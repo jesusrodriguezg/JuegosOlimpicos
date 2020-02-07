0e20f66c801307ef521ef10a592388c384c98fb1
VictorGarciaAcosta <34236650+VictorGarciaAcosta@users.noreply.github.com>
2020-02-07 10:27:26 +0100

    Add files via upload

---
+++ proyecto_JJOO/proyecto_antiguo/BBDD/Inserts.sql
@@ -0,0 +1,10 @@
+
+------ Evento Pregunta
+INSERT INTO `eventos_pregunta` (`ID`, `PREGUNTA`, `RESPUESTAS`, `RESPUESTA_ACERTADA`) VALUES
+(1, '¿En qué año se celebraron los JJOO en España?', '1992,2008,2000', '1992'),
+(2, '¿Cada cuántos años se celebran los JJOO?', '4,3,5', '4'),
+(3, '¿Dónde se celebraron los JJOO de 2016?', 'Rio de Janeiro,Londres,Madrid', 'Rio de Janeiro'),
+(4, '¿Qué nuevo deporte olímpica se introduce este año?','Skateboarding,Petanca,Pesca de trucha','Skateboarding'),
+(5, '¿Quién tiene el record mundial 100 metros lisos?','Usain Bolt,Carl Lewis,Speedy González','Usain Bolt'),
+(6, '¿En qué país se crearon los JJOO?','Grecia,EEUU,España','Grecia'),
+(7, '¿Cuál de estos deportes NO es disciplina olímpica?','Patinaje,Surf,Karate','Patinaje');
