2be7719c0744c3d19f9cee03bb223ef3f98e34e6
luicam <yuukyuu19@gmail.com>
2020-02-07 11:06:01 +0100

    cambios a ficheros 5

--- proyecto_JJOO/Proyecto_juego/BBDD/script.sql
+++ proyecto_JJOO/Proyecto_juego/BBDD/script.sql
@@ -11,12 +11,40 @@ create table 'eventos_pregunta'(
     RESPUESTAS VARCHAR(300),
     RESPUESTA_ACERTADA VARCHAR(100)
 );
+--
+-- Volcado de datos para la tabla `eventos_pregunta`
+--
+
+INSERT INTO `eventos_pregunta` (`ID`, `PREGUNTA`, `RESPUESTAS`, `RESPUESTA_ACERTADA`) VALUES
+(1, '¿En qué año se celebraron los JJOO en España?', '1992,2008,2000', '1992'),
+(2, '¿Cada cuántos años se celebran los JJOO?', '4,3,5', '4'),
+(3, '¿Dónde se celebraron los JJOO de 2016?', 'Rio de Janeiro,Londres,Madrid', 'Rio de Janeiro');
+
+-- --------------------------------------------------------
 
 --Crear tabla de eventos de puzzle
 create table 'evento_puzzle'(
     ID INT(3) PRIMARY KEY,
     RUTA_PUZZLE VARCHAR(100)
 );
+
+-- Volcado de datos
+INSERT INTO evento_puzzle(ID, RUTA_PUZZLE)
+VALUES(1, '../puzzles/p1/p1.js');
+
+INSERT INTO evento_puzzle(ID, RUTA_PUZZLE)
+VALUES(2, '../puzzles/p2/p2.js');
+
+INSERT INTO evento_puzzle(ID, RUTA_PUZZLE)
+VALUES(3, '../puzzles/p3/p3.js');
+
+INSERT INTO evento_puzzle(ID, RUTA_PUZZLE)
+VALUES(4, '../puzzles/p4/p4.js');
+
+INSERT INTO evento_puzzle(ID, RUTA_PUZZLE)
+VALUES(5, '../puzzles/p5/p5.js');
+-----------------------------------------------------------------
+
 
 --Crear tabla de eventos de imagen
 create table 'evento_imagen'(
