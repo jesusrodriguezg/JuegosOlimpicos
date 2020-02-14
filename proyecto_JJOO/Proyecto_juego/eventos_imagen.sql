e9e98027308fd56ce08add2392fedb3bb33aa34f
Jesús Rodríguez <sanedu@gmail.com>
2020-02-14 20:14:06 +0100

    Cambios
    Cambios en los inserts

--- proyecto_JJOO/Proyecto_juego/eventos_imagen.sql
+++ proyecto_JJOO/Proyecto_juego/eventos_imagen.sql
@@ -1,5 +1,5 @@
-INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (1,'¿Quién es este personaje, elegido como el embajador de los JJOO de Tokio?','****','../imagenes/eventos_imagen/goku.jpg','Goku');
-INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (2,'¿Cuál es el nombre de este tenista?','**** *****','../imagenes/eventos_imagen/nadal.jpg','Rafa Nadal');
-INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (3,'¿Cuál es el nombre de esta disciplina olímpica?','*******','../imagenes/eventos_imagen/curling.jpg','Curling');
-INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (4,'¿Cuál es el nombre de este nadador?','******* Phelps','../imagenes/eventos_imagen/phelps.jpg','Michael');
-INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (5,'¿Qué objeto es el que portan los atletas hasta el estadio olímpico?','********','../imagenes/eventos_imagen/antorcha.jpg','Antorcha');
+INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (1,'¿Quién es este personaje, elegido como el embajador de los JJOO de Tokio?','****','../imagenes/eventos_imagen/evento_imagen-1.jpg','Goku');
+INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (2,'¿Cuál es el nombre de este tenista?','**** *****','../imagenes/eventos_imagen/evento_imagen-2.jpg','Rafa Nadal');
+INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (3,'¿Cuál es el nombre de esta disciplina olímpica?','*******','../imagenes/eventos_imagen/evento_imagen-3.jpeg','Curling');
+INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (4,'¿Cuál es el nombre de este nadador?','******* Phelps','../imagenes/eventos_imagen/evento_imagen-4.jpg','Michael');
+INSERT INTO eventos_imagen (ID,PREGUNTA,AYUDA,IMAGEN,RESPUESTA_ACERTADA) VALUES (5,'¿Dónde se celebraron los últimos Juegos Olímpicos?','*** de Janeiro','../imagenes/eventos_imagen/evento_imagen-5.jpeg','Rio');
