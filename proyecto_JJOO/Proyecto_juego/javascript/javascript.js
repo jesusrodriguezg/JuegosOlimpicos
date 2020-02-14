b138dc675b4d5e853ed7bfc1a6a2bffa9457ac6f
Jesús Rodríguez <sanedu@gmail.com>
2020-02-14 07:45:24 +0100

    Añadidas funciones finales mostrarBotones() y métodos en PHP

--- proyecto_JJOO/Proyecto_juego/javascript/javascript.js
+++ proyecto_JJOO/Proyecto_juego/javascript/javascript.js
@@ -1,64 +1,75 @@
-//Imágenes a 240 * 240
-
 $(document).ready(function () {
-
-
+    
     /*
     * Se genera un evento de click por cada botón que se muestre
     * Se extrae el atributo "alt" de la imagen
     * Cuando se pulsa en el botón, se llama a slider.php y se le manda "alt" como parámetro
     * El script de PHP utiliza "alt" para hacer una select en la BD y devolver el CONTENIDO de la siguiente imagen
     * Con el contenido que devuelve  el script se crea el elemento con la siguiente imagen y todos sus atribtutos
+    * Se hace fadeOut del contenedor de respuestas, de los botones y de la imagen antigua y aparece la nueva
     */
-    $(".contenedor_opciones > ul > li").click(function(event){
+
+    cambiarImagen();
+
+    function cambiarImagen(){
+        $(".contenedor_opciones > ul > li").click(function(event){
             var alt=$(event.currentTarget).attr("alt");
             $.ajax({
-                url: "php/slider.php",
+                url: "php/metodos.php",
                 type: "POST",
-                data: {"alt":alt},
+                data: {"alt":alt,funcion:"funcion3"},
                 success: function (response) {
                     $(".fixed-bottom").fadeOut();
+                    $(".contenedor_opciones").fadeOut();
                     $(".active").append(response);
                     $(".active > img:first").fadeOut(1000,function(){
                         $(".active > img:first").remove();
-                    });                    
+                        //numRandom();
+
+                    });
                 }
             });
-        }); 
-
-    //Eventos originales para cambiar de imagen pulsando en los botones
-
-    /*$(".boton1").click(function(){
-        var numero = $(this).attr("alt");
-        var split = $(this).attr("class").split(" ");
-        var clase = split[1];
-        alert(clase);
-        $("#carouselExampleControls").carousel(parseInt(numero));
-    });
-    $(".boton2").click(function(){
-        var numero = $(this).attr("alt");
-        var split = $(this).attr("class").split(" ");
-        var clase = split[1];
-        alert(clase);
-        $("#carouselExampleControls").carousel(parseInt(numero));
-    });*/
-
+            mostrarBotones();
+        });
+        
+    }
 
     /*
-    Eleccion random para decidir si hay evento a la entrada de una pantalla o no
+    * Función que muestra los botones cuando se completa un evento
+    * Cogemos el atributo "id" de la imagen activa y lo pasamos por parámetros al archivo botones.php
+    * El script de PHP nos devuelve el CONTENIDO de tantos botones como correspondan a esa imagen
+    * Creamos un elemento <ul> con su correspondiente <li> por cada botón
+    * Mostramos el contenedor de los botones con un fadeIn
+    * Llamamos a cambiarImagen() para que le asigne eventos a los botones
     */
-    $(".opcion2_2_jugador").click(numRandom);
-    $(".opcion1_1_jugador").click(numRandom);
+
+    function mostrarBotones(){
+        var id=$(".active").attr("id");
+        $.ajax({
+            url: "php/metodos.php",
+            type: "POST",
+            data: {id:"id",funciones:"funcion4"},
+            success: function (response) {
+                $("contenedor_opciones > ul").remove(); 
+                var jsonBotones=JSON.parse(response);
+                for (var i = 0; i < response.length; i++) {
+                    $("contenedor_opciones").append("ul").addClass("list-group").append(response[i]);
+                }
+                $(".contenedor_opciones").fadeOut(1000,function(){
+                    $(".contenedor_opciones").fadeIn();
+                });
+            }
+        });
+        cambiarImagen();
+    }
 
     function numRandom() {
-        $("#contenedor_texto").css("display", "none");
-        $(".texto_contenedor").text("");
-        $("#respuestas").text("");
-
-        var numero = Math.floor(Math.random() * 10);
-        if (numero%2==0) {
+        //alert("entra");
+        let numero = Math.floor(Math.random() * 2);
+        /*if (numero+1%2==0) {
             generaEvento();
-        }
+        }*/
+        generaEvento();
     }
 
     /*
@@ -66,14 +77,15 @@ $(document).ready(function () {
     de un numero predefinido
     */
 
-    function generaEvento() {    
-        //var numero = Math.floor(Math.random() * 10);
-        let numero =1;
+    function generaEvento() { 
+        let numero = Math.floor(Math.random() * 2);
         switch (numero) {
-            case 1:
+            case 0:
             eventoPregunta();
             break;
-
+            case 1:
+            eventoImagen();
+            break;
             default:
             break;
         }    
@@ -84,48 +96,51 @@ $(document).ready(function () {
     un random (de nuevo) elegimos cual mostrar.
     */
 
-
-    function eventoPregunta() {    
-        var numero = Math.floor(Math.random() * 3);
+    function eventoPregunta() {   
+        let numero = Math.floor(Math.random() * (6 - 1)) + 1;
+        alert(numero);
         $.ajax({
-            url: "php/eventoPregunta.php",
-            type: "POST",
-            data: {numero:numero+1},
-            dataType: "JSON",
+            type: "post",
+            url: "php/metodos.php",
+            data: {numero:numero, funcion: "funcion1"},
+            //dataType: "JSON",
+            error(xhr,status,error){console.log("nope");
+            },
             success: function (jsonStr) {
-                muestrame(jsonStr);
+                let json = JSON.parse(jsonStr);
+                muestrame(json);
             }
-        });        
-       
+        });               
     }
 
     function muestrame(pregunta) {   
-        $("#contenedor_texto").css("display", "block");
-
-        $(".texto_contenedor").text(pregunta[0]);    
+       // $(".fixed-bottom:first-child").css("display", "block");
         let index = 1;
-        
+        let lista=$("<ul>");
+        lista.attr("class", "list-group");
+        lista.attr("id", "evt-pregunta");
+        let cuestion =$("<li>").text(pregunta[0]); 
+        cuestion.attr("class", "list-group-item");
+        lista.append(cuestion); 
+        //console.log(cuestion);
         pregunta[1].split(",").forEach(element => {
-            var boton =$("<button>").text(element); 
-            boton.attr("id", index);
-            boton.attr("class", "botonRespuesta");
-            var salto =$("<br>"); 
-            $("#respuestas").append(boton); 
-            $("#respuestas").append(salto); 
+            let elemento =$("<li>").text(element); 
+            elemento.attr("id", index);
+            elemento.attr("class", "respuesta list-group-item");
+            lista.append(elemento); 
+            //console.log(elemento);
             index ++;            
         });
-        var respuesta =$("<p>").text(pregunta[2]); 
-
+        $(".fixed-bottom").append(lista); 
+        let respuesta =$("<p>").text(pregunta[2]); 
         respuesta.attr("id", "acertada");
         respuesta.css("display", "none");  
-        $("#respuestas").append(respuesta); 
+        $(".fixed-bottom").append(respuesta); 
         escuchaRespuesta();
-
     }
 
-    function escuchaRespuesta() {
-        
-        var respuestas = document.getElementsByClassName("botonRespuesta");
+    function escuchaRespuesta() {        
+        let respuestas = document.getElementsByClassName("respuesta");
         for (let i = 0; i < respuestas.length; i++) {
             respuestas[i].addEventListener("click", eventoRespuesta, false);
         }
@@ -135,12 +150,18 @@ $(document).ready(function () {
      let respuesta = $(e.currentTarget).text();
      let acertada = $("#acertada").text();
      if (respuesta == acertada) {
-            alert("Has acertado");
-            //mostrarBotones
-            //cambioPantalla();
-        }else{
-            alert("Has fallado");
-        }
+        $("#evt-pregunta").remove();
+        alert("Has acertado");
+        let aciertos = sessionStorage.getItem('aciertosJuego');
+        aciertos++;
+        sessionStorage.setItem('aciertosJuego', aciertos);
+        //mostrarBotones
+        //cambioPantalla();
+    }else{
+        $("#evt-pregunta").remove();
+        alert("Has fallado");
+    }
+    $("#acertada").remove();
     }
 
     /*Función que muestra los botones una vez que se ha completado el evento (respondido a la pregunta, etc.)
@@ -160,4 +181,80 @@ $(document).ready(function () {
                 $(".texto_contenedor").text("VAYA QUE SITIO ES ESTE");
             });
      }
+
+     function eventoImagen(){
+        let numero = Math.floor(Math.random() * 5);
+        console.log("random imagen: "+numero);
+        $.ajax({
+            url: "php/metodos.php",
+            type: "POST",
+            data: {numero:numero,funcion: "funcion2"},
+            //dataType: "JSON",
+            success: function (jsonStr) {
+                let json = JSON.parse(jsonStr);
+                muestrameiMAGEN(json);
+            }
+        });     
+     }
+     function muestrameiMAGEN(imagen) {  
+        //console.log(imagen);
+        let lista=$("<ul>");
+        lista.attr("class", "list-group");
+        lista.attr("id", "evt-imagen");
+        let cuestion =$("<li>").text(imagen[0]); 
+        cuestion.attr("class", "list-group-item");
+        lista.append(cuestion);         
+        let elemento =$("<li>"); 
+        elemento.attr("class", "list-group-item");
+        let entrada = $("<input>");
+        entrada.attr("id", "respuesta");
+        entrada.attr("placeholder", imagen[1]);
+        elemento.append(entrada);
+        let boton = $("<button>").text("Responder");
+        boton.attr("id","responderImagen");
+        elemento.append(boton);
+        lista.append(elemento); 
+        $(".fixed-bottom").append(lista); 
+        let respuesta =$("<p>").text(imagen[3]); 
+        respuesta.attr("id", "acertada");
+        respuesta.css("display", "none");  
+        $(".fixed-bottom").append(respuesta); 
+        $(".contenedor_opciones .list-group").css("display", "none");
+
+        let cara = $("<img>").attr("src",imagen[2]);
+        cara.attr("id","imgPregunta");
+        $(".contenedor_opciones").append(cara);
+        $(".contenedor_opciones").css("left","20%");
+        $(".contenedor_opciones").css("top","5%");
+        escuchaImagen();
+    }
+
+    function escuchaImagen() {
+        let acertada = $("#acertada").text();    
+        acertada= acertada.toUpperCase();
+        $("#responderImagen").click( function (){
+            let respuesta = $("#respuesta").val();
+            respuesta =respuesta.toUpperCase();
+            if (acertada==respuesta) {
+                $("#evt-imagen").remove();
+                alert("Has acertado");
+                let aciertos = sessionStorage.getItem('aciertosJuego');
+                aciertos++;
+                sessionStorage.setItem('aciertosJuego', aciertos);
+            }else{
+                $("#evt-imagen").remove();
+                alert("Has fallado");
+            }
+            $("#imgPregunta").remove();
+            $("#acertada").remove();
+            $(".contenedor_opciones .list-group").css("display", "block");
+            $(".contenedor_opciones").removeAttr("style");       
+        });        
+    }
+
+    $(".botonFinal").click(function () {     
+        window.location="ending.html";
+    })
+
+
 })
